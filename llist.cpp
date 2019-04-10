502d398d8d099f3e1486ca47740804211f2953d0
Maliha Tabassum <45563960+Maliha006@users.noreply.github.com>
2019-04-09 21:57:41 -0400

    Update llist.cpp

--- llist.cpp
+++ llist.cpp
@@ -6,12 +6,24 @@ namespace nodespace{
 	
 	void LList::append(const node::value_type& entry){
             node* temp = new node(entry, NULL);
+		if(head_ptr == NULL)
+		{
+			head_ptr = temp;
+			tail_ptr = temp;
+			return;
+		}
+		
             tail_ptr->set_link(temp);
             tail_ptr = temp;
 	}
 	
 	void LList::insert(std::size_t position, const node::value_type& entry){
-        node* cursor = head_ptr;
+        
+		if(position < 0 || position > list_length)
+		{
+			throw std::out_of_range("Out of Range");
+		}
+	node* cursor = head_ptr;
         std::size_t i = 0;
         
         while(i < position)
@@ -98,17 +110,76 @@ namespace nodespace{
         head_ptr = NULL;
         tail_ptr = NULL;
         list_length = 0;
-	};
+	}
 
 	//returns new list
 	LList* LList::copy(){
-
-	};
+	
+	LList* new_list = new LList();
+	new_list->append(head_ptr->data);
+		
+	node* cursor = head_ptr->link();
+		
+	while(cursor != NULL)
+	{
+		LList->append(cursor->data);
+		cursor = cursor->link();
+	}
+	return new_list;
+	}
 
 	//additional functions to index into list
 	node* LList::operator[] (size_t ind){
+	node* temp = head_ptr;
+	size_t i = 1;
+		
+	while(i != ind)
+	{
+		temp = temp->link();
+		i++;
+	}
+	return temp;
+	}
 
-	};
-
-	std::list<std::tuple<node::value_type, int>> frequency(LList data);
+	std::list<std::tuple<node::value_type, int>> frequency(LList data){
+		std::tuple<node::value_type, int> cnt_node;
+		std::vector<node::value_type>used;
+		std::list<std::tuple<node::value_type, int>> list_mode;
+		
+		node* cursor = data->head_ptr();
+		
+		while(cursor!= NULL)
+		{
+			int t = 1;
+			
+			node* cursor2 = cursor;
+		        node::value_type v = cursor->data();
+			
+			while(cursor2 != NULL)
+			{
+				if(cursor2->data() == v)
+				{
+					t++;
+				}
+				cursor2 = cursor2->link();
+			}
+			bool not_found = true;
+			
+			for(int i = 0; i<v.size(); i++)
+			{
+				if(v[i] == cursor->data())
+					not_found = false;
+			}
+			
+			if(not_found)
+			{
+				cnt_node = make_tuple(cursor->data(), t);
+				list_mode.push_back(cnt_node);
+			}
+			
+			cursor = cursor->link();
+		}
+	}
+	
+	return list_mode;
 }
